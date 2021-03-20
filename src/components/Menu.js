126112df69c65647d3ae0505a65b381658cd4552
Karen9041 <karen010401@gmail.com>
2021-03-20 15:00:07 +0800

    first

---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+export default function Menu() {
+    return (
+        <div className="menu row flex-column">
+            <p className="text-light opacity-3 pt-3" >Tableware</p>
+            <p className="text-light opacity-3 pt-3">Cookware</p>
+            <p className="text-light opacity-3 pt-3">Home accessories</p>
+            <p className="text-light opacity-3 pt-3">Lighting</p>
+            <p className="text-light opacity-3 pt-3">Textile</p>
+            <p className="text-light opacity-3 pt-3">Furniture</p>
+        </div>
+    );
+
+}
